d428729f68951a901fe90049926526387352d60e
si777 <suren.islyaev@gmail.com>
2019-03-19 08:28:40 +0000

    Added uncommon frequencies to datafeeder

--- CODE/features/dataFeed.py
+++ CODE/features/dataFeed.py
@@ -1,13 +1,28 @@
 import pandas as pd
+import os
 
 class DataFeeder():
-    def __init__(self, fileLoc, batchSize, tokenizer):
-        self.dataLocation=fileLoc
+    def __init__(self, fileLoc, batchSize, tokenizer, max_freq = 3):
+        self.dataLocation = fileLoc
+        self.cacheLocation = fileLoc.split('.')[0]+'-cache.csv'
         self.batchSize = batchSize
         self.tokenizer = tokenizer
+        self.max_freq = max_freq
         self.nskips = 0
-        self.datasetSize = pd.read_csv(self.dataLocation).shape[0]
+        if self.cacheLocation.split('/')[-1] not in os.listdir('FILES/Datasets'):
+            self.clean_unk()
+        self.datasetSize = pd.read_csv(self.cacheLocation).shape[0]
         self.maxskips = self.datasetSize // self.batchSize
+
+    def clean_unk(self):
+        df = pd.read_csv(self.dataLocation)
+        from collections import Counter
+        index = list()
+        for row in df.iterrows():
+            if Counter(self.tokenizer.encode_input(row[1][1])).get(2.0,0)>=self.max_freq :
+                index.append(row[0])
+        df.loc[index,:].to_csv(self.cacheLocation, index=False)
+
 
     def preprocess_data(self, data):
         Xi = self.tokenizer.encode_input_sequences(data.values[:, 0])
@@ -17,7 +32,7 @@ class DataFeeder():
         return [Xi, Yi], Yo
 
     def genTrainBatch(self):
-        data = pd.read_csv(self.dataLocation, skiprows=self.batchSize*self.nskips, nrows=self.batchSize)
+        data = pd.read_csv(self.cacheLocation, skiprows=self.batchSize*self.nskips, nrows=self.batchSize)
         if self.nskips < self.maxskips-1:
             self.nskips+=1
         else:
@@ -26,6 +41,6 @@ class DataFeeder():
         return self.preprocess_data(data)
 
     def genValBatch(self, size):
-        return self.preprocess_data(pd.read_csv(self.dataLocation,
+        return self.preprocess_data(pd.read_csv(self.cacheLocation,
                                                 skiprows=self.batchSize*(self.maxskips-1),
                                                 nrows=size))
